37e5e601643e158d768a3740a7fef7b5ca04f464
Eryc Ferreira <eryc.ferreira@normatel.com.br>
2022-08-04 16:52:36 -0300

    change for https to running on vercel

--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from 'cors';
-import {createServer} from 'http';
+import {createServer} from 'https';
 
 import { routes } from './routes';
 import socket from './socket';
